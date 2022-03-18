ce4c93365b84478d79eaeb750bea9215c370a7c4
DmitryGalich <emeyl.prosto@mail.ru>
2022-03-18 22:05:39 +0300

    Simplifying server

--- libs/lib_network/src/server.cpp
+++ libs/lib_network/src/server.cpp
@@ -1,9 +1,13 @@
 #include "network.h"
 
 #include <arpa/inet.h>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
 #include <cstring>
 #include <fcntl.h>
 #include <mutex>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <thread>
 #include <unistd.h>
@@ -12,6 +16,9 @@
 #include "logger.h"
 
 namespace network {
+
+typedef int descriptor;
+static const descriptor kInvalidDesciptor{-1};
 
 class Server::Implementation {
 public:
@@ -22,26 +29,29 @@ public:
   void stop();
 
 private:
-  typedef int socket_descriptor;
+  struct Data {
+    struct ConnectingData {
+      struct sockaddr_in server_address_data_;
+      std::unique_ptr<std::thread> thread_;
+      std::atomic_bool is_need_cycling_{false};
+      std::vector<descriptor> clients_;
+      descriptor server_{kInvalidDesciptor};
+      descriptor epoll_{kInvalidDesciptor};
+      struct epoll_event event_;
+    } connecting_data_;
 
-  const int kMaxSocketsRequestsCount_{4096};
-  const int kInvalidSocketDesciptor{-1};
+    const int kMaxSocketsRequestsCount_{4096};
+
+  } data_;
 
   void run_connecting_thread(const std::string &ip, const uint32_t port);
-
-  std::vector<socket_descriptor> clients_;
-  socket_descriptor server_{kInvalidSocketDesciptor};
-  struct sockaddr_in server_address_data_;
-
-  std::unique_ptr<std::thread> connecting_thread_;
-  std::unique_ptr<std::thread> epoll_thread_;
 };
 
 void Server::Implementation::start(const std::string &ip, const uint32_t port) {
 
   log::info("starting...", __PRETTY_FUNCTION__);
 
-  connecting_thread_.reset(
+  data_.connecting_data_.thread_.reset(
       new std::thread([&]() { run_connecting_thread(ip, port); }));
 
   log::info("started", __PRETTY_FUNCTION__);
@@ -50,8 +60,12 @@ void Server::Implementation::start(const std::string &ip, const uint32_t port) {
 void Server::Implementation::stop() {
   log::info("stopping...", __PRETTY_FUNCTION__);
 
-  if (connecting_thread_)
-    connecting_thread_->join();
+  if (data_.connecting_data_.thread_) {
+    if (data_.connecting_data_.is_need_cycling_.load())
+      data_.connecting_data_.is_need_cycling_.store(false);
+
+    data_.connecting_data_.thread_->join();
+  }
 
   log::info("stopped", __PRETTY_FUNCTION__);
 }
@@ -60,56 +74,91 @@ void Server::Implementation::run_connecting_thread(const std::string &ip,
                                                    const uint32_t port) {
 
   const auto configure = [&]() {
-    server_address_data_.sin_family = AF_INET;
-    server_address_data_.sin_port = htons(port);
+    data_.connecting_data_.server_address_data_.sin_family = AF_INET;
+    data_.connecting_data_.server_address_data_.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, ip.c_str(), &(server_address_data_.sin_addr)) != 1)
+    if (inet_pton(AF_INET, ip.c_str(),
+                  &(data_.connecting_data_.server_address_data_.sin_addr)) != 1)
       throw std::runtime_error("inet_pton() - " + std::string(strerror(errno)));
 
-    server_ = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_ < 0)
+    data_.connecting_data_.server_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (data_.connecting_data_.server_ < 0)
       throw std::runtime_error("socket() - " + std::string(strerror(errno)));
 
-    //      if (fcntl(server_, F_SETFL, O_NONBLOCK) < 0)
-    //        handle_error("fcntl() - " + std::string(strerror(errno)));
+    if (fcntl(data_.connecting_data_.server_, F_SETFL, O_NONBLOCK) < 0)
+      throw std::runtime_error("fcntl() - " + std::string(strerror(errno)));
 
     int opt = 1;
-    if (setsockopt(server_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt)) < 0)
+    if (setsockopt(data_.connecting_data_.server_, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0)
       throw std::runtime_error("setsockopt() - " +
                                std::string(strerror(errno)));
 
-    if (bind(server_, (struct sockaddr *)&server_address_data_,
-             sizeof(server_address_data_)) < 0)
+    if (bind(data_.connecting_data_.server_,
+             (struct sockaddr *)&data_.connecting_data_.server_address_data_,
+             sizeof(data_.connecting_data_.server_address_data_)) < 0)
       throw std::runtime_error("bind() - " + std::string(strerror(errno)));
+
+    if (listen(data_.connecting_data_.server_,
+               data_.kMaxSocketsRequestsCount_) < 0)
+      throw std::runtime_error("listen() - " + std::string(strerror(errno)));
+
+    data_.connecting_data_.epoll_ = epoll_create(1);
+    if (data_.connecting_data_.epoll_ < 0)
+      throw std::runtime_error("epoll_create() - " +
+                               std::string(strerror(errno)));
+
+    data_.connecting_data_.event_.data.fd = data_.connecting_data_.server_;
+    data_.connecting_data_.event_.events = EPOLLIN | EPOLLOUT;
+    if (epoll_ctl(data_.connecting_data_.epoll_, EPOLL_CTL_ADD,
+                  data_.connecting_data_.server_,
+                  &data_.connecting_data_.event_) < 0)
+      throw std::runtime_error("epoll_ctl() - " + std::string(strerror(errno)));
   };
 
-  const auto run_connecting_cycle = [&]() {
-    while (true) {
-      if (listen(server_, kMaxSocketsRequestsCount_) < 0)
-        throw std::runtime_error("listen() - " + std::string(strerror(errno)));
+  const auto run_cycle = [&]() {
+    using namespace std::chrono_literals;
+    static const int kWaitingTimeout(0);
+
+    data_.connecting_data_.is_need_cycling_.store(true);
+
+    while (data_.connecting_data_.is_need_cycling_.load()) {
+      log::info("epoll_wait() attempt", __PRETTY_FUNCTION__);
+
+      const int kEventsCount = epoll_wait(
+          data_.connecting_data_.epoll_, &data_.connecting_data_.event_,
+          data_.kMaxSocketsRequestsCount_, kWaitingTimeout);
+      if (kEventsCount == -1)
+        throw std::runtime_error("epoll_wait() - " +
+                                 std::string(strerror(errno)));
+
+      std::this_thread::sleep_for(500ms);
     }
+
+    data_.connecting_data_.is_need_cycling_.store(false);
   };
 
   const auto make_stop = [&]() {
-    for (auto &client : clients_)
+    for (auto &client : data_.connecting_data_.clients_)
       close(client);
 
-    clients_.clear();
+    data_.connecting_data_.clients_.clear();
 
-    if (server_ >= 0)
-      close(server_);
+    if (data_.connecting_data_.server_ >= 0)
+      close(data_.connecting_data_.server_);
 
-    server_ = kInvalidSocketDesciptor;
+    data_.connecting_data_.server_ = kInvalidDesciptor;
   };
 
   // =======================================
 
   try {
     configure();
-    run_connecting_cycle();
+    run_cycle();
   } catch (const std::exception &exception) {
+    log::error("Connecting thread error", __PRETTY_FUNCTION__);
   }
+
   make_stop();
 }
 
--- libs/lib_network/tests/network_test.cpp
+++ libs/lib_network/tests/network_test.cpp
@@ -1,14 +1,35 @@
 #include "network.h"
 
+#include <chrono>
 #include <memory>
+#include <thread>
 
 #include "gtest/gtest.h"
 
+#include "logger.h"
+
 class NetworkTests : public ::testing::Test {
 protected:
-  void SetUp() override{};
-  void TearDown() override{};
+  void SetUp() override {
+    if (std::filesystem::is_directory(kTestsFolderPath_))
+      std::filesystem::remove_all(kTestsFolderPath_);
 
+    if (!std::filesystem::create_directory(kTestsFolderPath_))
+      throw std::runtime_error("Can't create directory \"" +
+                               std::string(kTestsFolderPath_) + "\"");
+
+    log::set_path(kTestsFolderPath_);
+  };
+  void TearDown() override {
+    log::clear_path();
+
+    if (std::filesystem::is_directory(kTestsFolderPath_))
+      std::filesystem::remove_all(kTestsFolderPath_);
+  };
+
+  const std::filesystem::path kTestsFolderPath_{
+      std::filesystem::temp_directory_path().c_str() +
+      std::string("/TestsFolder/")};
   network::Server server_;
 };
 
@@ -16,7 +37,9 @@ protected:
 // 8080)); }
 
 TEST_F(NetworkTests, Simple) {
+  using namespace std::chrono_literals;
   EXPECT_NO_THROW(server_.start("127.0.0.1", 8080));
+  std::this_thread::sleep_for(5s);
 }
 
 int main(int argc, char **argv) {
