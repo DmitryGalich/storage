7122a16ed390c40f3833675dbee79ee8084ee291
DmitryGalich <emeyl.prosto@mail.ru>
2022-03-22 00:23:45 +0300

    Parse arguments function

--- apps/console_server/main.cpp
+++ apps/console_server/main.cpp
@@ -4,9 +4,7 @@
 
 #include "logger.h"
 
-int main(int argc, char **argv) {
-  std::string log_path;
-
+int parse_arguments(const int argc, char **argv, std::string &log_path) {
   if (argc < 2) {
     std::cerr << "Not enough arguments. Need log folder path" << std::endl;
     return -1;
@@ -22,17 +20,25 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  {
-    const std::string kApplicationName(
-        console_server_PROJECT_NAME + std::string("_") +
-        std::to_string(console_server_VERSION_MAJOR) + std::string("_") +
-        std::to_string(console_server_VERSION_MINOR) + std::string("_") +
-        std::to_string(console_server_VERSION_PATCH));
+  return 0;
+}
 
-    log::set_application_name(kApplicationName);
-    log::set_path(log_path);
-    log::info(kApplicationName);
-  }
+int main(int argc, char **argv) {
+  std::string log_path;
+
+  int status = parse_arguments(argc, argv, log_path);
+  if (status != 0)
+    return status;
+
+  const std::string kApplicationName(
+      console_server_PROJECT_NAME + std::string("_") +
+      std::to_string(console_server_VERSION_MAJOR) + std::string("_") +
+      std::to_string(console_server_VERSION_MINOR) + std::string("_") +
+      std::to_string(console_server_VERSION_PATCH));
+
+  log::set_application_name(kApplicationName);
+  log::set_path(log_path);
+  log::info(kApplicationName);
 
   return 0;
 }
