a1617fd641e78e916351ac2909c55ce8a9c66d7d
Dmitry Galich <emeyl.prosto@mail.ru>
2023-01-03 13:18:57 +0300

    Reading file chunk crc

--- libs/crc_module/tests/crc_module_tests.cpp
+++ libs/crc_module/tests/crc_module_tests.cpp
@@ -34,6 +34,8 @@ struct CrcTestsHandler : public testing::Test
         return std::filesystem::exists(kFilepath);
     }
 
+    const std::filesystem::path kFilepath_{"file.txt"};
+    const std::string kContent{"Hello world"};
     const std::filesystem::path kTmpFolderPath_{std::filesystem::temp_directory_path().c_str() + static_cast<std::string>("/tests_folder/")};
 };
 
@@ -49,27 +51,63 @@ TEST_F(CrcTestsHandler, SameCrc)
     }
 
     {
-        const std::string kData{"Hello world"};
-
-        const auto kCrc0 = crc_module::compute_crc(kData.c_str());
-        const auto kCrc1 = crc_module::compute_crc(kData.c_str());
+        const auto kCrc0 = crc_module::compute_crc(kContent.c_str());
+        const auto kCrc1 = crc_module::compute_crc(kContent.c_str());
 
         EXPECT_EQ(kCrc0, kCrc1);
     }
 }
 
-TEST_F(CrcTestsHandler, FileCrc)
+TEST_F(CrcTestsHandler, FullFileContentCrcViaStringstream)
 {
-    const std::filesystem::path kFilepath("file.txt");
+    EXPECT_TRUE(CrcTestsHandler::create_file(kFilepath_, kContent));
 
-    EXPECT_TRUE(CrcTestsHandler::create_file(kFilepath, "Hello world"));
-
-    std::ifstream file(kTmpFolderPath_.c_str() + static_cast<std::string>(kFilepath));
+    std::ifstream file(kTmpFolderPath_.c_str() + static_cast<std::string>(kFilepath_));
     EXPECT_TRUE(file.is_open());
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
+    std::stringstream content_from_file;
+    content_from_file << file.rdbuf();
     file.close();
 
-    crc_module::compute_crc(buffer.str().c_str());
+    const auto kContentCrc = crc_module::compute_crc(kContent.c_str());
+    const auto kContentFromFilerc = crc_module::compute_crc(content_from_file.str().c_str());
+    EXPECT_EQ(kContentCrc, kContentFromFilerc);
+}
+
+TEST_F(CrcTestsHandler, FullFileContentCrcViaRead)
+{
+    EXPECT_TRUE(CrcTestsHandler::create_file(kFilepath_, kContent));
+
+    std::ifstream file(kTmpFolderPath_.c_str() + static_cast<std::string>(kFilepath_));
+    EXPECT_TRUE(file.is_open());
+
+    file.seekg(0, std::ios::end);
+    size_t size = file.tellg();
+    std::string content_from_file(size, ' ');
+    file.seekg(0);
+    file.read(&content_from_file[0], size);
+    file.close();
+
+    const auto kContentCrc = crc_module::compute_crc(kContent.c_str());
+    const auto kContentFromFilerc = crc_module::compute_crc(content_from_file.c_str());
+    EXPECT_EQ(kContentCrc, kContentFromFilerc);
+}
+
+TEST_F(CrcTestsHandler, FileChunkCrc)
+{
+    const std::string kFileChunk{"Hello"};
+
+    EXPECT_TRUE(CrcTestsHandler::create_file(kFilepath_, kContent));
+
+    std::ifstream file(kTmpFolderPath_.c_str() + static_cast<std::string>(kFilepath_));
+    EXPECT_TRUE(file.is_open());
+
+    std::string content_from_file(kFileChunk.size(), ' ');
+    file.seekg(0);
+    file.read(&content_from_file[0], kFileChunk.size());
+    file.close();
+
+    const auto kFileChunkCrc = crc_module::compute_crc(kFileChunk.c_str());
+    const auto kContentFromFilerc = crc_module::compute_crc(content_from_file.c_str());
+    EXPECT_EQ(kFileChunkCrc, kContentFromFilerc);
 }
