2e81b8247cc17cf57a2d4348198b5629dfefe017
Dmitry Galich <emeyl.prosto@mail.ru>
2023-01-10 23:35:56 +0300

    console app fixed

---
+++ apps/cloud_server_app/cloud_server_app_console_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "cmake_config.h"
+
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+int main()
+{
+    LOG(INFO) << cloud_server_app_PROJECT_NAME;
+    LOG(INFO) << "version " << cloud_server_app_VERSION_MAJOR << "." << cloud_server_app_VERSION_PATCH << "." << cloud_server_app_VERSION_PATCH;
+
+    return 0;
+}
--- apps/cloud_server_app/cmake_config.h.cmake
+++ apps/cloud_server_app/cmake_config.h.cmake
@@ -1,4 +1,4 @@
-#define cloud_server_console_app_PROJECT_NAME "@PROJECT_NAME@"
-#define cloud_server_console_app_VERSION_MAJOR @cloud_server_console_app_VERSION_MAJOR@
-#define cloud_server_console_app_VERSION_MINOR @cloud_server_console_app_VERSION_MINOR@
-#define cloud_server_console_app_VERSION_PATCH @cloud_server_console_app_VERSION_PATCH@
+#define cloud_server_app_PROJECT_NAME "@PROJECT_NAME@"
+#define cloud_server_app_VERSION_MAJOR @cloud_server_app_VERSION_MAJOR@
+#define cloud_server_app_VERSION_MINOR @cloud_server_app_VERSION_MINOR@
+#define cloud_server_app_VERSION_PATCH @cloud_server_app_VERSION_PATCH@
