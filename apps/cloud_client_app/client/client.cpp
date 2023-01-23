3f5cfe59d824576a1fd00f26e36d8d09786533fd
Dmitry Galich <emeyl.prosto@mail.ru>
2023-01-23 23:40:36 +0300

    synchroned client first work

--- apps/cloud_client_app/client/client.cpp
+++ apps/cloud_client_app/client/client.cpp
@@ -2,6 +2,16 @@
 
 #include "easylogging++.h"
 
+#include "oatpp/core/base/Environment.hpp"
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
+#include "oatpp/web/client/HttpRequestExecutor.hpp"
+#include "oatpp/network/tcp/client/ConnectionProvider.hpp"
+
+#include "DemoApiClient.hpp"
+
+#include "AsyncExample.hpp"
+#include "SimpleExample.hpp"
+
 namespace cloud
 {
     Client::Client()
@@ -16,11 +26,21 @@ namespace cloud
 
     void Client::start()
     {
+        oatpp::base::Environment::init();
+        auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+        auto connectionProvider = oatpp::network::tcp::client::ConnectionProvider::createShared({"httpbin.org", 80});
+        auto requestExecutor = oatpp::web::client::HttpRequestExecutor::createShared(connectionProvider);
+        auto client = DemoApiClient::createShared(requestExecutor, objectMapper);
+
         LOG(INFO) << "Started";
+
+        SimpleExample::runExample(client);
     }
 
     void Client::stop()
     {
+        oatpp::base::Environment::destroy();
+
         LOG(INFO) << "Stopped";
     }
 }
--- apps/cloud_server_app/server/server.cpp
+++ apps/cloud_server_app/server/server.cpp
@@ -2,19 +2,11 @@
 
 #include "easylogging++.h"
 
-#include "oatpp/web/server/AsyncHttpConnectionHandler.hpp"
-#include "oatpp/web/server/HttpRouter.hpp"
-#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
-#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
-#include "oatpp/core/macro/component.hpp"
-
 namespace cloud
 {
     Server::Server()
     {
         LOG(DEBUG) << "Constructor";
-
-        // oatpp::network::Server server;
     }
 
     Server::~Server()
@@ -24,6 +16,7 @@ namespace cloud
 
     void Server::start()
     {
+
         LOG(INFO) << "Started";
     }
 
