121ddf95f54816afc2429fe8126bd1e54e8bd486
Dmitry Galich <emeyl.prosto@mail.ru>
2023-01-25 20:49:00 +0300

    Sync requests

--- apps/cloud_client_app/client/internal/oatpp_client/oatpp_client.cpp
+++ apps/cloud_client_app/client/internal/oatpp_client/oatpp_client.cpp
@@ -2,11 +2,15 @@
 
 #include "easylogging++.h"
 
+#include "oatpp/core/async/Executor.hpp"
 #include "oatpp/core/base/Environment.hpp"
 #include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 #include "oatpp/network/tcp/client/ConnectionProvider.hpp"
+#include "oatpp/web/client/HttpRequestExecutor.hpp"
 
 #include "../abstract_client.h"
+
+#include "api_client.hpp"
 
 namespace cloud
 {
@@ -27,6 +31,37 @@ namespace cloud
 
             auto object_mapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
             auto connection_provider = oatpp::network::tcp::client::ConnectionProvider::createShared({"httpbin.org", 80});
+            auto request_executor = oatpp::web::client::HttpRequestExecutor::createShared(connection_provider);
+            auto client = DemoApiClient::createShared(request_executor, object_mapper);
+            constexpr static const char *TAG = "SimpleExample";
+
+            {
+                auto data = client->doGet()->readBodyToString();
+                OATPP_LOGD(TAG, "[doGet] data='%s'", data->c_str());
+            }
+
+            {
+                auto data = client->doPost("Some data passed to POST")->readBodyToString();
+                OATPP_LOGD(TAG, "[doPost] data='%s'", data->c_str());
+            }
+
+            {
+                auto data = client->doGetAnything("path-parameter")->readBodyToString();
+                OATPP_LOGD(TAG, "[doGetAnything] data='%s'", data->c_str());
+            }
+
+            {
+                auto data = client->doPostAnything("path-parameter", "Some body here")->readBodyToString();
+                OATPP_LOGD(TAG, "[doPostAnything] data='%s'", data->c_str());
+            }
+
+            {
+                auto dto = RequestDto::createShared();
+                dto->message = "Some message";
+                dto->code = 200;
+                auto data = client->doPostWithDto(dto)->readBodyToString();
+                OATPP_LOGD(TAG, "[doPostWithDto] data='%s'", data->c_str());
+            }
 
             LOG(INFO) << "OATPP Started";
         }
