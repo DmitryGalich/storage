72efaa363287caf1ce294bc174d30296980b659f
Dmitry Galich <emeyl.prosto@mail.ru>
2023-02-10 21:22:29 +0300

    listener from example

--- apps/cloud_server_app/server/internal/oatpp_server/oatpp_server.cpp
+++ apps/cloud_server_app/server/internal/oatpp_server/oatpp_server.cpp
@@ -9,8 +9,116 @@
 #include "oatpp/web/server/AsyncHttpConnectionHandler.hpp"
 #include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 #include "oatpp-websocket/AsyncConnectionHandler.hpp"
+#include "oatpp-websocket/AsyncWebSocket.hpp"
 
 #include "../abstract_server.h"
+
+namespace
+{
+    /**
+     * WebSocket listener listens on incoming WebSocket events.
+     */
+    class WSListener : public oatpp::websocket::AsyncWebSocket::Listener
+    {
+
+    private:
+        /**
+         * Buffer for messages. Needed for multi-frame messages.
+         */
+        oatpp::data::stream::BufferOutputStream m_messageBuffer;
+
+    public:
+        /**
+         * Called on "ping" frame.
+         */
+        CoroutineStarter onPing(const std::shared_ptr<AsyncWebSocket> &socket, const oatpp::String &message) override
+        {
+            // OATPP_LOGD("Server_WSListener", "onPing");
+            return socket->sendPongAsync(message);
+        }
+
+        /**
+         * Called on "pong" frame
+         */
+        CoroutineStarter onPong(const std::shared_ptr<AsyncWebSocket> &socket, const oatpp::String &message) override
+        {
+            // OATPP_LOGD("Server_WSListener", "onPong");
+            return nullptr; // do nothing
+        }
+
+        /**
+         * Called on "close" frame
+         */
+        CoroutineStarter onClose(const std::shared_ptr<AsyncWebSocket> &socket, v_uint16 code, const oatpp::String &message) override
+        {
+            // OATPP_LOGD("Server_WSListener", "onClose code=%d", code);
+            return nullptr; // do nothing
+        }
+
+        /**
+         * Called on each message frame. After the last message will be called once-again with size == 0 to designate end of the message.
+         */
+        CoroutineStarter readMessage(const std::shared_ptr<AsyncWebSocket> &socket, v_uint8 opcode, p_char8 data, oatpp::v_io_size size) override
+        {
+            if (size == 0)
+            { // message transfer finished
+
+                auto wholeMessage = m_messageBuffer.toString();
+                m_messageBuffer.setCurrentPosition(0);
+
+                // OATPP_LOGD("Server_WSListener", "onMessage message='%s'", wholeMessage->c_str());
+
+                /* Send message in reply */
+                return socket->sendOneFrameTextAsync("Hello from oatpp!: " + wholeMessage);
+            }
+            else if (size > 0)
+            { // message frame received
+                m_messageBuffer.writeSimple(data, size);
+            }
+
+            return nullptr; // do nothing
+        }
+    };
+}
+
+namespace
+{
+    /**
+     * Listener on new WebSocket connections.
+     */
+    class WSInstanceListener : public oatpp::websocket::AsyncConnectionHandler::SocketInstanceListener
+    {
+    public:
+        /**
+         * Counter for connected clients.
+         */
+        std::atomic<int> sockets_;
+
+    public:
+        /**
+         *  Called when socket is created
+         */
+        void onAfterCreate_NonBlocking(const std::shared_ptr<WSListener::AsyncWebSocket> &socket, const std::shared_ptr<const ParameterMap> &params) override
+        {
+            sockets_++;
+            OATPP_LOGD("Server_WSInstanceListener", "New Incoming Connection. Connection count=%d", sockets_.load());
+
+            /* In this particular case we create one WSListener per each connection */
+            /* Which may be redundant in many cases */
+            socket->setListener(std::make_shared<WSListener>());
+        }
+
+        /**
+         *  Called before socket instance is destroyed.
+         */
+        void onBeforeDestroy_NonBlocking(const std::shared_ptr<WSListener::AsyncWebSocket> &socket) override
+        {
+            sockets_--;
+            OATPP_LOGD("Server_WSInstanceListener", "Connection closed. Connection count=%d", sockets_.load());
+        }
+    };
+
+}
 
 namespace cloud
 {
@@ -39,6 +147,7 @@ namespace cloud
             std::shared_ptr<oatpp::network::ConnectionHandler> connection_handler_;
             std::shared_ptr<oatpp::data::mapping::ObjectMapper> object_mapper_;
             std::shared_ptr<oatpp::websocket::AsyncConnectionHandler> socket_connection_handler_;
+            std::shared_ptr<oatpp::websocket::AsyncConnectionHandler::SocketInstanceListener> socket_listener_;
             std::shared_ptr<oatpp::network::Server> server_;
         };
 
@@ -102,6 +211,16 @@ namespace cloud
                 return false;
             }
 
+            socket_listener_.reset();
+            socket_listener_ = std::make_shared<WSInstanceListener>();
+            if (!socket_listener_)
+            {
+                LOG(ERROR) << "SocketInstanceListener is not created";
+                return false;
+            }
+
+            socket_connection_handler_->setSocketInstanceListener(socket_listener_);
+
             server_.reset();
             server_ = oatpp::network::Server::createShared(connection_provider_, connection_handler_);
             if (!server_)
@@ -137,6 +256,7 @@ namespace cloud
                 server_->stop();
 
             server_.reset();
+            socket_listener_.reset();
             socket_connection_handler_.reset();
             object_mapper_.reset();
             connection_handler_.reset();
