8feb8b0907f5d04ee55769fca64a36a11988b691
Dmitry Galich <emeyl.prosto@mail.ru>
2023-03-11 17:20:37 +0300

    it works, but code is not good yet

--- apps/server/server/internal/oatpp_server/oatpp_server.cpp
+++ apps/server/server/internal/oatpp_server/oatpp_server.cpp
@@ -2,6 +2,12 @@
 
 #include "easylogging++.h"
 
+#include "oatpp/core/base/Environment.hpp"
+#include "oatpp/network/Server.hpp"
+
+#include "AppComponent.hpp"
+#include "./controller/MyController.hpp"
+
 namespace cloud
 {
     namespace internal
@@ -28,6 +34,28 @@ namespace cloud
 
         bool OatppServer::OatppServerImpl::init()
         {
+            oatpp::base::Environment::init();
+
+            /* Register Components in scope of run() method */
+            AppComponent components;
+
+            /* Get router component */
+            OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+
+            /* Create MyController and add all of its endpoints to router */
+            router->addController(std::make_shared<MyController>());
+
+            /* Get connection handler component */
+            OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler, "http");
+
+            /* Get connection provider component */
+            OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+
+            /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
+            oatpp::network::Server server(connectionProvider, connectionHandler);
+
+            /* Run server */
+            server.run();
 
             return true;
         }
@@ -50,6 +78,7 @@ namespace cloud
 
         void OatppServer::OatppServerImpl::stop()
         {
+            oatpp::base::Environment::destroy();
         }
     }
 }
