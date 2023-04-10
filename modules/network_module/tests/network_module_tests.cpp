e97d3902756fba3b7d9ce4cead9b269e61b4d215
Dmitry Galich <emeyl.prosto@mail.ru>
2023-04-10 22:43:34 +0300

    cmake files variables

--- modules/network_module/tests/network_module_tests.cpp
+++ modules/network_module/tests/network_module_tests.cpp
@@ -38,9 +38,9 @@ TEST_F(ServerTests, SimpleTest)
 
     const auto kProcessorsCores = std::thread::hardware_concurrency();
     const auto kProcessorsCoresForServer = (kProcessorsCores > 1) ? (kProcessorsCores - 1) : 1;
+    const auto kConfig = network_module::Server::Config::load_config(kConfigPath);
 
     network_module::Server server(kProcessorsCoresForServer);
-
-    EXPECT_TRUE(server.start({}));
+    EXPECT_TRUE(server.start(kConfig));
     server.stop();
 }
