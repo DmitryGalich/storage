105da2b5e1d2f112661a30693a9bc9bb03b2cdae
Dmitry Galich <emeyl.prosto@mail.ru>
2023-04-29 16:50:04 +0300

    creating client

---
+++ apps/client/client_console_app/client_console_app_main.cpp
@@ -0,0 +1,22 @@
+#include "configs/cmake_config.h"
+
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+void configure_logger()
+{
+    el::Configurations config(CMAKE_CURRENT_SOURCE_DIR + std::string{"/configs/log_config.conf"});
+    el::Loggers::reconfigureAllLoggers(config);
+}
+
+int main()
+{
+    configure_logger();
+
+    LOG(INFO) << "================================";
+    LOG(INFO) << PROJECT_NAME;
+    LOG(INFO) << "version " << PROJECT_VERSION;
+
+    return 0;
+}
