077d38261d93106afd26324eb268528988c74471
Dmitry Galich <emeyl.prosto@mail.ru>
2023-06-02 21:50:58 +0300

    global exception handler

--- apps/client/client_console_app/client_console_app_main.cpp
+++ apps/client/client_console_app/client_console_app_main.cpp
@@ -37,52 +37,60 @@ void wait_for_user_command()
 
 int main()
 {
-    configure_logger();
-
-    LOG(INFO) << "================================";
-    LOG(INFO) << PROJECT_NAME;
-    LOG(INFO) << "version " << PROJECT_VERSION;
-
-    std::promise<void> client_promise;
-    auto client_future = client_promise.get_future();
-
-    storage::client::Client client(std::move(client_promise));
-
     try
     {
-        if (!client.start(CMAKE_CURRENT_SOURCE_DIR + std::string{"/configs/client_config.json"}))
+        configure_logger();
+
+        LOG(INFO) << "================================";
+        LOG(INFO) << PROJECT_NAME;
+        LOG(INFO) << "version " << PROJECT_VERSION;
+
+        std::promise<void> client_promise;
+        auto client_future = client_promise.get_future();
+
+        storage::client::Client client(std::move(client_promise));
+
+        try
         {
-            LOG(ERROR) << "Can't start client";
+            if (!client.start(CMAKE_CURRENT_SOURCE_DIR + std::string{"/configs/client_config.json"}))
+            {
+                LOG(ERROR) << "Can't start client";
+                LOG(INFO) << "Shutting down the application";
+                client.stop();
+                return -1;
+            }
+        }
+        catch (const std::exception &e)
+        {
+            LOG(ERROR) << e.what();
             LOG(INFO) << "Shutting down the application";
             client.stop();
             return -1;
         }
+
+        std::future<void> user_command_future = std::async(&wait_for_user_command);
+        while (true)
+        {
+            if (user_command_future.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
+            {
+                LOG(INFO) << "Signal to stop from user command";
+                break;
+            }
+
+            if (client_future.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
+            {
+                LOG(INFO) << "Signal to stop from client command";
+                break;
+            }
+        }
+
+        client.stop();
     }
     catch (const std::exception &e)
     {
-        LOG(ERROR) << e.what();
-        LOG(INFO) << "Shutting down the application";
-        client.stop();
-        return -1;
+        std::cerr << e.what() << '\n';
+        LOG(ERROR) << "Global error: " << e.what();
     }
-
-    std::future<void> user_command_future = std::async(&wait_for_user_command);
-    while (true)
-    {
-        if (user_command_future.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
-        {
-            LOG(INFO) << "Signal to stop from user command";
-            break;
-        }
-
-        if (client_future.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
-        {
-            LOG(INFO) << "Signal to stop from client command";
-            break;
-        }
-    }
-
-    client.stop();
 
     exit(0);
 }
